16c8ca373265b9abfd7d28873f27314e73fd13ad
Deepak <gannamanenideepak@gmail.com>
2026-04-11 18:24:46 +0530

    Refactor eslint.config.js by removing unused options

--- frontend/src/eslint.config.js
+++ frontend/src/eslint.config.js
@@ -11,19 +11,6 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+      reactRefresh.configs.vite,]
   },
 ])
